90c71f3a5f08d3a2662ceb78070cba96948bb15a
Naman Sharma <namansharma1112004@gmail.com>
2026-03-30 16:04:15 +0530

    added factorial solution

---
+++ BasicMaths/factorial.java
@@ -0,0 +1,17 @@
+package BasicMaths;
+
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+
+        int factorial = 1 ;
+        for(int i = 1 ; i<=n;i++){
+            factorial=factorial*i;
+        }
+
+        System.out.println(factorial);
+    }
+}
